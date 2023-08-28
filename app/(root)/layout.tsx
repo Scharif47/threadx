b212aa6aca8a46b3d43a8ef017734566b24e7fad
Scharif47 <scharif47@outlook.de>
2023-08-28 23:49:37 +0200

    Topbar added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,10 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
+
+import Topbar from "@/components/shared/Topbar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Bottombar from "@/components/shared/Bottombar";
 import "../globals.css";
 
 // Metadata for Search Engine Optimization (SEO)
@@ -21,7 +26,19 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+        <body className={`${inter.className}`}>
+          <Topbar />
+          <main>
+            <LeftSidebar />
+
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+
+            <RightSidebar />
+          </main>
+          <Bottombar />
+        </body>
       </html>
     </ClerkProvider>
   );
