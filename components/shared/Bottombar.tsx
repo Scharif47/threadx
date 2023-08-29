6004b9ee95370b25034b3a19bd3350455899c999
Scharif47 <scharif47@outlook.de>
2023-08-29 22:47:03 +0200

    LeftSidebar & Bottombar added

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,5 +1,46 @@
+// Set as client-side
+"use client";
+
+import { sidebarLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+
 function Bottombar() {
-  return <h1>Bottombar</h1>
+  const pathname = usePathname();
+
+  return (
+    <section className="bottombar">
+      <div className="bottombar_container">
+        {sidebarLinks.map((link) => {
+          // Check if the current route is active in the sidebar
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+
+          // Return Sidebar Elements
+          return (
+            <Link
+              key={link.label}
+              href={link.route}
+              className={`bottombar_link ${isActive && "bg-primary-500"}`}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+              <p className="text-subtle-medium text-light-1 max-sm:hidden">
+                {/* Use only first word on tablet */}
+                {link.label.split(/\s+.+/)[0]}
+              </p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
 
 export default Bottombar;
--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,5 +1,62 @@
+// Set as client-side
+"use client";
+
+import { SignOutButton, SignedIn } from "@clerk/nextjs";
+import { sidebarLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+
 function LeftSidebar() {
-  return <h1>LeftSidebar</h1>
+  const router = useRouter();
+  const pathname = usePathname();
+
+  return (
+    <section className="custom-scrollbar leftsidebar">
+      <div className="flex w-full flex-1 flex-col gap-6 px-6">
+        {sidebarLinks.map((link) => {
+          // Check if the current route is active in the sidebar
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+
+          // Return Sidebar Elements
+          return (
+            <Link
+              key={link.label}
+              href={link.route}
+              className={`leftsidebar_link ${isActive && "bg-primary-500"}`}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+              <p className="text-light-1 max-lg:hidden">{link.label}</p>
+            </Link>
+          );
+        })}
+      </div>
+
+      <div className="mt-10 px-6">
+        <SignedIn>
+          <SignOutButton signOutCallback={() => router.push("/sign-in")}>
+            <div className="flex cursor-pointer gap-4 p-4">
+              <Image
+                src="/assets/logout.svg"
+                alt="logout"
+                width={24}
+                height={24}
+              />
+
+              <p className="text-light-2 max-lg:hidden">Logout</p>
+            </div>
+          </SignOutButton>
+        </SignedIn>
+      </div>
+    </section>
+  );
 }
 
 export default LeftSidebar;
